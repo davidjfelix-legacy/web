a52e6384137628332203a4e71cd4efe39c36d4e5
David J Felix <felix.davidj@gmail.com>
2016-08-04 16:56:32 -0400

    Use bootstrap from cdn and build video container

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Footer from './components/Footer';
+import PageHeader from './components/PageHeader';
+import VideoContainer from './components/VideoContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <PageHeader></PageHeader>
+        <VideoContainer
+          videoWidth={480}
+          videoSources={["https://s3.amazonaws.com/buukkittt/bbb_sunflower_1080p_60fps_normal.mp4"]}></VideoContainer>
+        <Footer></Footer>
       </div>
     );
   }
 }
 
 export default App;
+
