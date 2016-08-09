58d6666e254c87ace1be21f6b759dd67e17377ae
David J. Felix <DavidJFelix@users.noreply.github.com>
2016-08-09 09:31:45 -0400

    Add Proptypes and expand previewslist

--- src/components/VideoPreviewsList.js
+++ src/components/VideoPreviewsList.js
@@ -6,13 +6,25 @@ import VideoPreview from './VideoPreview';
 class VideoPreviewsList extends Component {
   render() {
     return (
-      <div></div>
+      <ul className="VideoPreviewsList">
+        {this.props.videoPreviews.map((videoPreview, index) => ()
+          <li key={index}>
+            <VideoPreview {...videoPreview} />
+          <li>
+        ))}
+      </ul>
     );
   }
 }
 
 VideoPreviewsList.propTypes = {
+  videosPreviews: React.PropTypes.arrayOf(
+    React.PropTypes.shape({
+      videoId: React.PropTypes.string.isRequired,
+      videoTitle: React.PropTypes.string.isRequired,
+      videoDescription: React.PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired
 }
 
 export default VideoPreviewsList;
-
