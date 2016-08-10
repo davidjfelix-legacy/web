0a6a8623a2ba76e88718fb733b6f87fffca58dda
David J Felix <felix.davidj@gmail.com>
2016-08-09 20:58:18 -0400

    Work on Home view links

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 class HomeView extends Component {
+  constructor() {
+    super();
+    this.state = {
+      videos: ["hello", "world"]
+    };
+  }
+  
   render() {
     return (
       <div>
-        <Link to={"v/1"}>Video Link</Link>
+        {this.state.videos.map((video, index) => (
+          <Link to={"v/1"} key={index}>Video Link</Link>
+        ))}
       </div>
     );
   }
