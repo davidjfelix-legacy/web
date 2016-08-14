36e4d2b2810ff744cd86ea1abddc05e69d57646c
David J Felix <felix.davidj@gmail.com>
2016-08-14 16:35:31 -0400

    Quit using bootstrap

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import './VideoContainer.css';
+
 
 class VideoContainer extends Component {
   render() {
     return (
-      <div>
+      <div className="VideoContainer">
         <video width={this.props.videoWidth.toString()} controls>
           {this.props.videoSources.map((videoSource, index) => (
             <source src={videoSource} key={index} />
--- src/components/VideoView.js
+++ src/components/VideoView.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 
 import VideoContainer from './VideoContainer';
 
-import './App.css';
+import './VideoView.css';
 
 
 class VideoView extends Component {
   render() {
     return (
-      <div className="container">
+      <div>
         <VideoContainer
           videoWidth={960}
           videoSources={["https://s3.amazonaws.com/buukkittt/bbb_sunflower_1080p_60fps_normal.mp4"]}></VideoContainer>
