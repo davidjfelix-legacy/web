760a9310eea7ceb3c2c4d99e15c43659e9fd5486
David J Felix <felix.davidj@gmail.com>
2016-08-21 11:35:51 -0400

    Make a kinda ghetto comment stream dump

--- src/components/CommentStream.js
+++ src/components/CommentStream.js
@@ -4,9 +4,17 @@ import React, { Component } from 'react';
 class CommentStream extends Component {
   render() {
     return (
-      <ul></ul>
+      <ul>
+        {this.props.events.map((event, index) => (
+          <li key={index}>{JSON.stringify(event)}</li>
+        ))}
+      </ul>
     );
   }
+}
+
+CommentStream.propTypes = {
+  events: React.PropTypes.arrayOf(React.PropTypes.object).isRequired
 }
 
 export default CommentStream;
