0cae594ad79c3ab3ca589a238601925f4248740c
David J Felix <felix.davidj@gmail.com>
2016-08-23 23:27:45 -0400

    Move routes into index. It makes sense.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import { Route, Router, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 
+import App from './components/App';
+import GroupView from './components/GroupView';
+import HomeView from './components/HomeView';
+import UserView from './components/UserView';
+import VideoView from './components/VideoView';
 import { configure as configureStore } from './state/store';
-import routes from './config/routes';
 
 import './index.css';
 
@@ -14,7 +18,17 @@ const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>{routes}</Router>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <Route path="v/:videoId" component={VideoView} />
+        <Route path="videos/:videoId"  component={VideoView} />
+        <Route path="u/:userName" component={UserView} />
+        <Route path="users/:userName" component={UserView} />
+        <Route path="g/:groupName" component={GroupView} />
+        <Route path="groups/:groupName" component={GroupView} />
+        <IndexRoute component={HomeView} />
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
