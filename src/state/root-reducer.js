031e44aae50fb189bcda1df171977940a5ca25d6
David J Felix <felix.davidj@gmail.com>
2016-09-02 22:11:21 -0400

    Add redux responsive to the app

--- src/state/root-reducer.js
+++ src/state/root-reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import { responsiveStateReducer } from 'redux-responsive';
 import commentStream from './modules/commentStream';
 import streamingPoll from './modules/streamingPoll';
 import videoContainer from './modules/videoContainer';
 
 const mg4App = combineReducers({
+  browser: responsiveStateReducer,
   commentStream,
   streamingPoll,
   videoContainer,
--- src/state/store.js
+++ src/state/store.js
@@ -1,17 +1,21 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import createLogger from 'redux-logger';
 import promiseMiddleware from 'redux-promise';
+import { responsiveStoreEnhancer } from 'redux-responsive';
 import thunkMiddleware from 'redux-thunk';
 import { routerMiddleware } from 'react-router-redux'
 import { browserHistory } from 'react-router'
 
 import mg4App from './root-reducer';
 
-const finalCreateStore = applyMiddleware(
-  routerMiddleware(browserHistory),
-  thunkMiddleware,
-  promiseMiddleware,
-  createLogger({ collapsed: true })
+const finalCreateStore = compose(
+  responsiveStoreEnhancer,
+  applyMiddleware(
+    routerMiddleware(browserHistory),
+    thunkMiddleware,
+    promiseMiddleware,
+    createLogger({ collapsed: true }),
+  )
 )(createStore);
 
 let store;
