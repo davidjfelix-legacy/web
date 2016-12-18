ba8724a151c6c7e250a2bb251e84c1a14e0bd351
David J Felix <felix.davidj@gmail.com>
2016-12-17 23:54:36 -0500

    Remove the storyboard stuff that doesn't matter

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import { storiesOf, action, linkTo } from '@kadira/storybook';
-import Button from './Button';
-import Welcome from './Welcome';
+import LoginOrSignupForm from '../components/LoginOrSignupForm';
 
-storiesOf('Welcome', module)
-  .add('to Storybook', () => (
-    <Welcome showApp={linkTo('Button')}/>
-  ));
-
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+storiesOf('LoginOrSignupForm', module)
+  .add('Basic Form', () => (
+    <LoginOrSignupForm />
   ));
