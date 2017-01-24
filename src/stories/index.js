b71df9ba3cd24bf7bf6a20d691d3ee3615102a68
David J Felix <felix.davidj@gmail.com>
2017-01-24 06:50:38 -0500

    Remove storybook because it doesn't work with css modules

--- src/stories/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf, action, linkTo } from '@kadira/storybook';
-
-import Comment from '../components/Comment';
-
-storiesOf('Comment', module)
-  .add('Default', () => (
-    <Comment username="ExampleUser" text="This is a comment."/>
-  ));
