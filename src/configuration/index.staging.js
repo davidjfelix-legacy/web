d97a6729a079d7af710d6d81efc49cbe56597db4
David J Felix <felix.davidj@gmail.com>
2017-04-26 11:35:56 -0400

    Set prod config for all envs

--- src/configuration/index.staging.js
+++ src/configuration/index.staging.js
@@ -1,5 +1,15 @@
-export const FACEBOOK_CLIENT_ID = "1940062226221485"
-export const FIREBASE_CONFIG = {}
+export const FACEBOOK_CLIENT_ID = "1932441930316848"
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBhYoODN5Mt-3RMt9beTtbM2-N9r7ACfUI",
+  authDomain: "iotv-1e541.firebaseapp.com",
+  databaseURL: "https://iotv-1e541.firebaseio.com",
+  projectId: "iotv-1e541",
+  storageBucket: "iotv-1e541.appspot.com",
+  messagingSenderId: "790062285088"
+}
 export const MG4_API_BASE_URL = "http://api.mg4.tv"
-export const MG4_WEB_BASE_URL = "https://mg4-staging.herokuapp.com"
-export const REACT_REDUX_FIREBASE_CONFIG = {}
+export const MG4_WEB_BASE_URL = "https://mg4.herokuapp.com"
+export const REACT_REDUX_FIREBASE_CONFIG = {
+  userProfile: 'users',
+  enableLogging: true
+}
