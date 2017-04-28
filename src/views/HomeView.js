5a76b60f8671e90a3d3e134915153177d149fd07
David J Felix <felix.davidj@gmail.com>
2017-04-27 23:04:10 -0400

    Get an ugly json printout of videos

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,41 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { firebaseConnect, isLoaded, isEmpty, dataToJS } from 'react-redux-firebase'
 
-import MenuLayout from '../components/MenuLayout';
-import VideoPreviewsList from '../containers/VideoPreviewsList';
 
+import MenuLayout from '../components/MenuLayout'
+import VideoPreviewsList from '../containers/VideoPreviewsList'
+
+
+const mapStateToProps = ({ firebase }) => ({
+  videos: dataToJS(firebase, 'videos')
+})
 
 class HomeView extends Component {
   constructor() {
-    super();
-    this.state = {
-      videos: [
-        {
-          videoLinkURL: "/v/1",
-          videoThumbnailURL: "https://placekitten.com/g/400/225",
-          videoTitle: "A video hello",
-          videoUser: "hellouser",
-          videoUserLinkURL: "/u/1",
-        },
-        {
-          videoLinkURL: "v/1",
-          videoThumbnailURL: "https://placekitten.com/g/400/225",
-          videoTitle: "A video world",
-          videoUser: "worlduser",
-          videoUserLinkURL: "/u/1",
-        }
-      ]
-    };
+    super()
   }
 
   render() {
     return (
       <MenuLayout>
         <div>
-          <VideoPreviewsList videoPreviews={this.state.videos} />
+          {JSON.stringify(this.props.videos)}
         </div>
       </MenuLayout>
-    );
+    )
   }
 }
 
-export default HomeView;
+export default firebaseConnect(['videos'])(connect(mapStateToProps)(HomeView))
