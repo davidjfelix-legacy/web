7d910d712de2df1db94f9ebf3b5acd93935daeb2
David J Felix <felix.davidj@gmail.com>
2017-05-14 17:36:18 -0400

    Revert "Add auth HOC"
    This reverts commit 1fb3310d6841fd44844940b2b4f5b0cf70248fe4.

--- src/components/hocs.js
+++ src/components/hocs.js
@@ -1,12 +1,10 @@
 import { branch, lifecycle, renderComponent } from 'recompose'
 
-import auth from '../auth'
 import database from '../database'
 
 export const withDatabaseSubscribe = (trigger, getRefPath, getOnTrigger) => (
   lifecycle({
     componentWillMount() {
-      super.componentWillMount()
       this.databaseRef = database.ref(getRefPath(this.props))
       this.onTrigger = this.databaseRef.on(
         trigger,
@@ -14,24 +12,7 @@ export const withDatabaseSubscribe = (trigger, getRefPath, getOnTrigger) => (
       )
     },
     componentWillUnmount() {
-      super.componentWillUnmount()
       this.databaseRef.off(trigger, this.onTrigger)
-    },
-  })
-)
-
-export const withAuthSubscribe = (onAuthStateChanged) => (
-  lifecycle({
-    componentWillMount() {
-      super.componentWillMount()
-      this.unsubscribeAuth = auth.onAuthStateChanged((user) => (
-        onAuthStateChanged(user, this.props)
-      ))
-    },
-
-    componentWillUnmount() {
-      super.componentWillUnmount()
-      this.unsubscribeAuth()
     },
   })
 )
