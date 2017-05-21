6caa3f98de2e667bcfe04773db77a87b86cd1fa7
David J Felix <felix.davidj@gmail.com>
2017-05-20 23:38:27 -0400

    Change logo and header size

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -6,27 +6,28 @@ import { connect } from 'react-redux'
 export const styles = {
   pageHeader: {
     alignItems: 'center',
-    backgroundColor: "#212121",
+    backgroundColor: '#212121',
     display: 'flex',
-    height: '2.5em',
+    height: '3em',
     justifyContent: 'space-between',
     width: '100%',
   },
   brand: {
-    color: "white",
-    backgroundColor: "#212121",
+    color: 'white',
+    backgroundColor: '#212121',
     display: 'block',
-    fontFamily: "Arvo",
-    fontSize: "1.5em",
+    fontFamily: 'Arvo',
+    fontSize: '1.5em',
     fontWeight: 700,
-    fontStyle: "italic",
+    fontStyle: 'italic',
     paddingLeft: '.5em',
-    textDecoration: "none",
-    textWeight: "1px black",
-    width: '65px',
+    textDecoration: 'none',
+    textWeight: '1px black',
+    width: '80px',
   },
   profile: {
-    color: "white",
+    color: 'white',
+    paddingRight: '.5em'
   },
   text: {
     paintOrder: 'stroke',
@@ -66,23 +67,23 @@ const mapStateToProps = ({auth}) => ({
 
 const PageHeader = ({auth}) => (
   <nav style={styles.pageHeader}>
-    <Link style={styles.brand} to="/">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 45">
+    <Link style={styles.brand} to='/'>
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 45'>
         <defs>
-          <linearGradient id="grad1" x1="0%" y1="20%" x2="0%" y2="85%">
-            <stop offset="0%" style={styles.gradient.blue} />
-            <stop offset="40%" style={styles.gradient.blue} />
-            <stop offset="40.1%" style={styles.gradient.lightBlue} />
-            <stop offset="50%" style={styles.gradient.lightBlue} />
-            <stop offset="50.1%" style={styles.gradient.violet} />
-            <stop offset="63%" style={styles.gradient.violet} />
-            <stop offset="63.1%" style={styles.gradient.magenta} />
-            <stop offset="79%" style={styles.gradient.magenta} />
-            <stop offset="79.1%" style={styles.gradient.pink} />
-            <stop offset="100%" style={styles.gradient.pink} />
+          <linearGradient id='grad1' x1='0%' y1='20%' x2='0%' y2='85%'>
+            <stop offset='0%' style={styles.gradient.blue} />
+            <stop offset='40%' style={styles.gradient.blue} />
+            <stop offset='40.1%' style={styles.gradient.lightBlue} />
+            <stop offset='50%' style={styles.gradient.lightBlue} />
+            <stop offset='50.1%' style={styles.gradient.violet} />
+            <stop offset='63%' style={styles.gradient.violet} />
+            <stop offset='63.1%' style={styles.gradient.magenta} />
+            <stop offset='79%' style={styles.gradient.magenta} />
+            <stop offset='79.1%' style={styles.gradient.pink} />
+            <stop offset='100%' style={styles.gradient.pink} />
           </linearGradient>
         </defs>
-        <text x="5" y="40" fontFamily="Arvo" fontWeight="bold" fontStyle="italic" fontSize="40" fill="url(#grad1)" style={styles.text}>iotv</text>
+        <text x='5' y='40' fontFamily='Arvo' fontWeight='bold' fontStyle='italic' fontSize='40' fill='url(#grad1)' style={styles.text}>iotv</text>
       </svg>
     </Link>
     {auth.user !== null ?
