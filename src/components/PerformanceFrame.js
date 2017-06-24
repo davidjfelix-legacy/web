532b740d1023d022591e295f1654688bd5f4133c
David J Felix <felix.davidj@gmail.com>
2017-06-24 19:55:17 -0400

    Work on making performance frame great again

--- src/components/PerformanceFrame.js
+++ src/components/PerformanceFrame.js
@@ -1,14 +1,39 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { compose, withContext, withHandler, withState } from 'recompose'
 
 import VideoStream from './VideoStream'
 
+const context = {
+  videosState: PropTypes.object.isRequired
+}
 
-const PerformanceFrame = ({layout}) => (
+const enhance = compose(
+  withState('videosState', 'updateVideoState', {
+    time: 0,
+    videoState: 'PLAYING',
+  }),
+  withContext(
+    context,
+    (props) => ({
+      videoState: props.videoState
+    })
+  ),
+)
+
+const PerformanceFrame = ({layout, size}) => (
   <div>
-    {layout['videoStreams'].map((videoStream, index) => (
-      <VideoStream video={{url: videoStream.url}} style={{position: "absolute", zIndex: videoStream.z_index}} scale={videoStream.scale} key={index} />
-    ))}
+    <div style={{...size}}>
+      {layout['videoStreams'].map((videoStream, index) => (
+        <VideoStream
+          video={{url: videoStream.url}}
+          style={{position: "absolute", zIndex: videoStream.z_index}}
+          scale={videoStream.scale}
+          key={videoStream.url} />
+      ))}
+    </div>
+    <div>play</div>
   </div>
 )
 
-export default PerformanceFrame
+export default enhance(PerformanceFrame)
