083e70184ecaf412be7e53c0bd730b81e37abdb9
David J Felix <felix.davidj@gmail.com>
2017-07-23 17:47:39 -0400

    Create style for active navigation in usernav

--- src/components/UserNav.js
+++ src/components/UserNav.js
@@ -7,11 +7,17 @@ const styles = {
     flexDirection: 'row',
     padding: "1em",
   },
+  activeLink: {
+    borderBottom: "2px solid #BBDEFB",
+    padding: "1em",
+    textDecoration: "none",
+  },
   link: {
     borderBottom: "2px solid #222222",
     padding: "1em",
     textDecoration: "none",
-}}
+  },
+}
 
 export const navLinks = {
   overview: "Overview",
@@ -26,14 +32,38 @@ export const navLinks = {
 
 const UserNav = ({baseUrl, active}) => (
   <nav style={styles.nav}>
-    <Link style={styles.link} to={baseUrl}>Overview</Link>
-    <Link style={styles.link} to={`${baseUrl}/videos`}>{navLinks.overview}</Link>
-    <Link style={styles.link} to={`${baseUrl}/organizations`}>{navLinks.organizations}</Link>
-    <Link style={styles.link} to={`${baseUrl}/following`}>{navLinks.following}</Link>
-    <Link style={styles.link} to={`${baseUrl}/followers`}>{navLinks.followers}</Link>
-    <Link style={styles.link} to={`${baseUrl}/shows`}>{navLinks.shows}</Link>
-    <Link style={styles.link} to={`${baseUrl}/series`}>{navLinks.series}</Link>
-    <Link style={styles.link} to={`${baseUrl}/playlists`}>{navLinks.playlists}</Link>
+    {active === navLinks.overview?
+      <Link style={styles.activeLink} to={baseUrl}>{navLinks.overview}</Link>:
+      <Link style={styles.link} to={baseUrl}>{navLinks.overview}</Link>
+    }
+    {active === navLinks.videos?
+      <Link style={styles.activeLink} to={`${baseUrl}/videos`}>{navLinks.videos}</Link>:
+      <Link style={styles.link} to={`${baseUrl}/videos`}>{navLinks.videos}</Link>
+    }
+    {active === navLinks.organizations?
+      <Link style={styles.activeLink} to={`${baseUrl}/organizations`}>{navLinks.organizations}</Link>:
+      <Link style={styles.link} to={`${baseUrl}/organizations`}>{navLinks.organizations}</Link>
+    }
+    {active === navLinks.following?
+      <Link style={styles.activeLink} to={`${baseUrl}/following`}>{navLinks.following}</Link>:
+      <Link style={styles.link} to={`${baseUrl}/following`}>{navLinks.following}</Link>
+    }
+    {active === navLinks.followers?
+      <Link style={styles.activeLink} to={`${baseUrl}/followers`}>{navLinks.followers}</Link>:
+      <Link style={styles.link} to={`${baseUrl}/followers`}>{navLinks.followers}</Link>
+    }
+    {active === navLinks.shows?
+      <Link style={styles.activeLink} to={`${baseUrl}/shows`}>{navLinks.shows}</Link>:
+      <Link style={styles.link} to={`${baseUrl}/shows`}>{navLinks.shows}</Link>
+    }
+    {active === navLinks.series?
+      <Link style={styles.activeLink} to={`${baseUrl}/series`}>{navLinks.series}</Link>:
+      <Link style={styles.link} to={`${baseUrl}/series`}>{navLinks.series}</Link>
+    }
+    {active === navLinks.playlists?
+      <Link style={styles.activeLink} to={`${baseUrl}/playlists`}>{navLinks.playlists}</Link>:
+      <Link style={styles.link} to={`${baseUrl}/playlists`}>{navLinks.playlists}</Link>
+    }
   </nav>
 )
 
