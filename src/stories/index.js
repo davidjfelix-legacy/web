f8a84bef583dd53a1d5992eabaefc506f7131819
David J Felix <felix.davidj@gmail.com>
2017-08-06 17:41:34 -0400

    Add page header to storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { linkTo } from '@storybook/addon-links'
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'
 
 import configureStore from '../store'
 import PerformanceFrame from '../components/PerformanceFrame'
 import VideoStream from '../components/VideoStream'
+import PageHeader from "../components/PageHeader"
 
 const store = configureStore()
 
@@ -46,5 +47,14 @@ storiesOf('VideoStream', module)
   .add("4:3 Small VideoStream", () => (
     <Provider store={store} >
       <VideoStream video={{url:""}} aspectRatio={4/3} scaleWidth={720} />
+    </Provider>
+  ))
+
+storiesOf('PageHeader', module)
+  .add("Wide PageHeader", () => (
+    <Provider store={store} >
+      <div>
+        <PageHeader />
+      </div>
     </Provider>
   ))
