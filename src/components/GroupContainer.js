7b83a1677ea6e9556359dbb4a74994753f7dc892
David J Felix <felix.davidj@gmail.com>
2017-08-28 22:56:56 -0400

    Add a group view

--- src/components/GroupContainer.js
+++ src/components/GroupContainer.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React from 'react'
+import {connect} from 'react-redux'
+import {compose, withProps} from 'recompose'
 
+import {withDatabaseSubscribe} from './hocs'
+import {updateGroup} from '../actions/groups'
 
-const GroupContainer = () => (
-  <div></div>
-);
+const mapStateToProps = ({auth, groups}) => ({
+  groups
+})
 
-export default GroupContainer;
+const enhance = compose(
+  connect(mapStateToProps),
+  withProps(({match}) => ({
+    groupName: match.params.groupName
+  })),
+  withDatabaseSubscribe(
+    'value',
+    (props) => (`groups/${props.groupName}`),
+    (props) => (snapshot) => (props.dispatch(updateGroup({
+        groupId: props.groupName,
+        groupSnapshot: snapshot,
+      }
+    )))
+  )
+)
+
+const GroupContainer = ({groupName, groups}) => (
+  <div>{JSON.stringify(groups)}</div>
+)
+
+export default enhance(GroupContainer)
