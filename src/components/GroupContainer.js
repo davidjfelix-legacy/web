5bcffc95f56f17485630077148544d9c43b6ea82
David J Felix <felix.davidj@gmail.com>
2017-08-29 22:12:48 -0400

    Fill out group container with navigation links

--- src/components/GroupContainer.js
+++ src/components/GroupContainer.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {Route, Switch} from 'react-router'
+import {NavLink} from 'react-router-dom'
 import {compose, withProps} from 'recompose'
 
-import {withDatabaseSubscribe} from './hocs'
 import {updateGroup} from '../actions/groups'
+import {withDatabaseSubscribe} from './hocs'
 
 const mapStateToProps = ({auth, groups}) => ({
   groups
@@ -25,8 +27,23 @@ const enhance = compose(
   )
 )
 
-const GroupContainer = ({groupName, groups}) => (
-  <div>{JSON.stringify(groups)}</div>
+const GroupContainer = ({groupName, groups, match}) => (
+  <div>
+    <NavLink to={`${match.url}/members`}>members</NavLink>
+    <NavLink to={`${match.url}/roles`}>roles</NavLink>
+    <NavLink to={`${match.url}/series`}>series</NavLink>
+    <NavLink to={`${match.url}/shows`}>members</NavLink>
+    <NavLink to={`${match.url}/performances`}>performances</NavLink>
+    <NavLink to={`${match.url}/videos`}>videos</NavLink>
+    <Switch>
+      <Route path={`${match.url}/members`}/>
+      <Route path={`${match.url}/roles`}/>
+      <Route path={`${match.url}/series`}/>
+      <Route path={`${match.url}/shows`}/>
+      <Route path={`${match.url}/performances`}/>
+      <Route path={`${match.url}/videos`}/>
+    </Switch>
+  </div>
 )
 
 export default enhance(GroupContainer)
