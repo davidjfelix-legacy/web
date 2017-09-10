77030bd69fdba772e4632443ea0328ffc4f3370b
David J Felix <felix.davidj@gmail.com>
2017-09-10 12:42:23 -0400

    Add form for role view

--- src/components/GroupRolesView.js
+++ src/components/GroupRolesView.js
@@ -1,7 +1,56 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {compose, withHandlers, withProps, withState} from 'recompose'
+import {addMemberToRole, addRoleToGroup} from '../actions/groups'
 
-const GroupRolesView = () => (
-  <div/>
+
+const mapStateToProps = ({groups}) => ({
+  groups
+})
+
+const enhance = compose(
+  connect(mapStateToProps),
+  withProps(({match}) => ({
+    groupId: match.params.groupId
+  })),
+  withState('newRole', 'updateNewRole', {roleName: ''}),
+  withHandlers({
+    onNewRoleNameChange: props => event => {
+      props.updateNewRole({
+        ...props.newRole,
+        roleName: event.target.value,
+      })
+    },
+    onNewRoleSubmit: props => event => {
+      event.preventDefault()
+      props.dispatch(addRoleToGroup({
+        groupId: props.groupId,
+        roleName: props.newRoleName,
+      }))
+    }
+  }),
 )
 
-export default GroupRolesView
+const GroupRolesView = ({
+                          groups,
+                          groupId,
+                          onNewRoleNameChange,
+                          onNewRoleSubmit,
+                          newRole,
+                        }) => (
+  <div>
+    <form onSubmit={onNewRoleSubmit}>
+      <textarea
+        placeholder='New Role Name'
+        value={newRole.roleName}
+        onChange={onNewRoleNameChange}
+      />
+      <input
+        type='submit'
+        value='submit'
+      />
+    </form>
+  </div>
+)
+
+export default enhance(GroupRolesView)
