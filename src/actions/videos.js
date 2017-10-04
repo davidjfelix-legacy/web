f8a3104804aa433499f10e60d8567f21f77302c3
David J Felix <felix.davidj@gmail.com>
2017-10-03 22:41:37 -0400

    Start fleshing out some actions for video creation

--- src/actions/videos.js
+++ src/actions/videos.js
@@ -1,9 +1,29 @@
-import keyMirror from 'keymirror';
+import database from '../database'
 
-export const actionTypes = keyMirror({
-  UPDATE_VIDEO: null,
-  UPDATE_VIDEOS: null,
-})
+
+export const actionTypes = {
+  CREATE_VIDEO: 'CREATE_VIDEO',
+  UPDATE_VIDEO: 'UPDATE_VIDEO',
+  UPDATE_VIDEOS: 'UPDATE_VIDEOS',
+}
+
+export const createVideo = ({video, videoOwnerType, ownerId}) => {
+  const videoList = database.ref('videos')
+  const videoRef = videoList.push()
+  videoRef.set(
+    {
+      ...video,
+      owner_id: ownerId,
+      video_owner_type: videoOwnerType
+    }
+  )
+  return {
+    type: actionTypes.CREATE_GROUP,
+    video,
+    videoOwnerType,
+    ownerId,
+  }
+}
 
 export const updateVideo = ({videoId, videoSnapshot}) => ({
   type: actionTypes.UPDATE_VIDEO,
