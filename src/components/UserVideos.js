e762c50fba4e25c6cff3152ab499c6ff310c95ee
David J Felix <felix.davidj@gmail.com>
2017-10-08 12:15:48 -0400

    Make a user create video button

--- src/components/UserVideos.js
+++ src/components/UserVideos.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {compose} from 'recompose'
+import {compose, withHandlers, withProps} from 'recompose'
 
 import {updateUserVideos} from '../actions/userVideos'
+import {createVideo, VideoOwnerTypes} from '../actions/videos'
 import {withDatabaseSubscribe} from './hocs'
 import VideoPreviewsList from './VideoPreviewsList'
+
 
 const mapStateToProps = ({userVideos}) => ({
   userVideos,
@@ -13,23 +14,43 @@ const mapStateToProps = ({userVideos}) => ({
 
 const enhanceSubs = compose(
   connect(mapStateToProps),
+  withProps(({match}) => ({
+    userId: match.params.userId
+  })),
+  withHandlers(
+    {
+      onNewVideoSubmit: props => event => {
+        event.preventDefault()
+        props.dispatch(createVideo(
+          {
+            videoOwnerType: VideoOwnerTypes.USER_VIDEO,
+            ownerId: props.userId
+          })
+        )
+      }
+    }
+  ),
   withDatabaseSubscribe(
     'value',
     (props) => (`user-videos/${props.userId}`),
     (props) => (snapshot) => {
-      props.dispatch(updateUserVideos({
-        userId: props.userId,
-        userVideosSnapshot: snapshot.val(),
-      }))
+      props.dispatch(updateUserVideos(
+        {
+          userId: props.userId,
+          userVideosSnapshot: snapshot.val(),
+        }))
     }
   ),
 )
 
-const UserVideos = ({baseUrl, isEditable, userId, userVideos}) => (
+const UserVideos = ({baseUrl, isEditable, onNewVideoSubmit, userId, userVideos}) => (
   <div>
-    {isEditable ?
-      <Link to='/videos/new'>New</Link> : ''
-    }
+    <form onSubmit={onNewVideoSubmit}>
+      <input
+        type='submit'
+        value='submit'
+      />
+    </form>
     <VideoPreviewsList videoIds={userVideos[userId] ? Object.keys(userVideos[userId]) : []}/>
   </div>
 )
