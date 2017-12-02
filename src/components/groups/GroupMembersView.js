11f53c795f52467c92d31d820d40ec066e9d77be
David J Felix <felix.davidj@gmail.com>
2017-12-01 19:24:34 -0500

    Start working on the video view

--- src/components/groups/GroupMembersView.js
+++ src/components/groups/GroupMembersView.js
@@ -1,5 +1,5 @@
-import _ from 'lodash'
-import React from 'react'
+import * as _ from 'lodash'
+import * as React from 'react'
 import {connect} from 'react-redux'
 import {compose, withHandlers, withProps, withState} from 'recompose'
 
--- src/components/videos/VideoView.js
+++ src/components/videos/VideoView.js
@@ -1,12 +1,55 @@
+import * as _ from 'lodash'
 import * as React from 'react'
+import {connect} from 'react-redux'
+import {compose, withProps} from 'recompose'
 
+import {refreshVideo} from '../../actions/videos'
+import {withDatabaseSubscribe, withLoading, withNotFound} from '../hocs'
 import LoadingView from '../LoadingView'
+import NotFoundView from '../NotFoundView'
 
 
-const VideoView = () => (
+const mapStateToProps = ({videos}) => ({
+  videos
+})
+
+const enhance = compose(
+  connect(mapStateToProps),
+  withProps(({match}) => ({
+    videoId: match.params.videoId
+  })),
+  withDatabaseSubscribe(
+    'value',
+    ({videoId}) => (`videos/${videoId}`),
+    ({dispatch, videoId}) => (snapshot) => (
+      dispatch(refreshVideo(
+        {
+          videoId: videoId,
+          videoSnapshot: snapshot.val()
+        }
+      ))
+    )
+  ),
+  withLoading(
+    ({videoId, videos}) => !(_.has(videos, videoId)),
+    LoadingView
+  ),
+  withNotFound(
+    ({videoId, videos}) => (_.isNull(_.get(videos, videoId, null))),
+    NotFoundView
+  )
+)
+
+
+const VideoView = (
+  {
+    videoId,
+    videos,
+  }
+) => (
   <div>
     <LoadingView/>
   </div>
 )
 
-export default VideoView
+export default enhance(VideoView)
